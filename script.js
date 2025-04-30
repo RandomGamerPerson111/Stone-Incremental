decf9b30a9c311d4454f3ae5a66fbc78a8c2bea1
RandomGamerPerson111 <noperson.png@gmail.com>
2025-04-29 18:23:25 -0700

    Update and rename main.js to script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const baseplayer{
+  stone: 0
+  SPS: 0
+}
+
+player = baseplayer
+
+function update() {
+  player.stone+=player.SPS/20
+  document.getElementById("stone").textContent = player.stone.toFixed(2)
+}
