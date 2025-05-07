05e8bf46af6a64cabdc3738cc5c5c4e71804aef8
RandomGamerPerson111 <noperson.png@gmail.com>
2025-05-07 13:03:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ player = baseplayer
 
 function update() {
   player.stone+=player.SPS/(1000/updateinterval)
-  document.getElementById("stone").textContent = player.stone.toFixed(2) || 0
+  document.getElementById("stone").textContent = player.stone.toFixed(2) || "Error stone didn't load"
 }
 
 setInterval(update, updateinterval)
