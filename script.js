d56f4171804912dfb497e75f1bada0a341a15f56
RandomGamerPerson111 <noperson.png@gmail.com>
2025-05-07 12:45:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ player = baseplayer
 
 function update() {
   player.stone+=player.SPS/(1000/updateinterval)
-  document.getElementById("stone").textContent = player.stone.toFixed(2) or 0
+  document.getElementById("stone").textContent = player.stone.toFixed(2) || 0
 }
 
 setInterval(update, updateinterval)
