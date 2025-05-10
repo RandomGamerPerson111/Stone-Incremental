1aa7dfd2afa408ccd78ba0848bd16d30272b0c02
Cica <98735748+CicaProductions@users.noreply.github.com>
2025-05-10 23:13:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,13 +14,15 @@ function increaseSPS() {
   player.SPS += 1
 }
 
-
+function runchecks() {
+  if (player.MagmaUnlocked === 0 && player.Stone >= 1000) {player.MagmaUnlocked = 1}
+}
 
 function update() {
   player.Stone+=player.SPS/(1000/updateinterval)
-  if (player.MagmaUnlocked === 0 && player.Stone >= 1000) {player.MagmaUnlocked = 1}
   if (player.Stone >= 1000) {player.MagmaToGain = Math.floor(Math.cbrt(player.Stone/1000))} else {player.MagmaToGain = 0}
   document.getElementById("stone").textContent = player.Stone.toFixed(2) || "Error stone didn't load"
 }
 
 setInterval(update, updateinterval)
+setInterval(runchecks, 3000)
